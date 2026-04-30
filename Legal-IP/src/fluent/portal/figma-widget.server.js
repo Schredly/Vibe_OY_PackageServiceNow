edcb6dd857bc1b8553d41b673c64147021b4c339
Schredly Schrederson <144973458+Schredly@users.noreply.github.com>
2026-04-30 09:57:03 -0700

    Delete Legal-IP directory

--- Legal-IP/src/fluent/portal/figma-widget.server.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    data.title = "Legal IP Requests";
-
-    try {
-        data.scope = (gs.getCurrentScopeName && gs.getCurrentScopeName()) || '';
-    } catch (e) {
-        data.scope = '';
-    }
-})()
